6bc65c7cbf542f933a1903df858673ea31fc520a
cr08 <Vchat20@gmail.com>
2022-11-01 18:40:11 -0400

    Update 0_main_videos.py
    Added 'Upload' video type from Twitch to the video download script

--- 0_main_videos.py
+++ 0_main_videos.py
@@ -428,3 +428,152 @@ for idx, user in enumerate(users):
             if os.path.exists(file_path_render_tmp):
                 shutil.move(file_path_render_tmp, file_path_render) 
             print("\t- done in " + str(time.time() - t0) + " seconds")
+
+    # loop through each highlight and download
+    for video in arr_upload:
+
+        # check if we should download any more
+        if utils.terminated_requested:
+            print('terminate requested, not downloading any more..')
+            break
+
+        # DATA: api data of this vod
+        t0_start = time.time()
+        video_data = {
+            'id': video['helix']['id'],
+            'user_id': video['helix']['user_id'],
+            'user_name': video['helix']['user_name'],
+            'title': video['helix']['title'],
+            'type': video['helix']['type'],
+            'duration': video['helix']['duration'],
+            'url': video['helix']['url'],
+            'views': video['helix']['view_count'],
+            'moments': utils.get_vod_moments(video['helix']['id']),
+            'muted_segments': (video['helix']['muted_segments'] if video['helix']['muted_segments'] != None else []),
+            'recorded_at': video['helix']['created_at'].strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'recorded_at_iso': video['helix']['created_at'].strftime('%Y%m%d T%H%M%SZ')
+        }
+
+        # providing a single source for all filename calls in this script, including stripping illegal characters
+        filename_format = utils.cleanFilename(str(video_data['recorded_at_iso']) + " - " + str(video['helix']['id']) + " - " + str(video['helix']['title']) + "_" + str(video['helix']['type']))
+
+        # extract what folder we should save into
+        # create the folder if it isn't created already
+        try:
+            date = datetime.strptime(video_data['recorded_at'], '%Y-%m-%dT%H:%M:%SZ')
+            export_folder = format(date.year, '02') + "-" + format(date.month, '02') + "/"
+        except:
+            export_folder = "unknown/"
+        if not os.path.exists(path_data + export_folder):
+            os.makedirs(path_data + export_folder)
+
+        # VIDEO: check if the file exists
+        file_path_info = path_data + export_folder + filename_format + "_info.json"
+        print("\t- saving video info: " + file_path_info)
+        if not utils.terminated_requested and not os.path.exists(file_path_info):
+            with open(file_path_info, 'w', encoding="utf-8") as file:
+                json.dump(video_data, file, indent=4)
+        elif not utils.terminated_requested:
+            print("\t- updating video info: " + file_path_info)
+            with open(file_path_info) as f:
+                video_info = json.load(f)
+            # update moments if failed before
+            if len(video_info["moments"]) == 0:
+                moments = utils.get_vod_moments(video['helix']['id'])
+                if len(moments) != 0:
+                    video_info["moments"] = moments
+            # finally write to file
+            with open(file_path_info, 'w', encoding="utf-8") as file:
+                json.dump(video_info, file, indent=4)
+
+        # VIDEO: check if the file exists
+        file_path = path_data + export_folder + filename_format + ".mp4"
+        print("\t- download video: " + file_path)
+        if not utils.terminated_requested and not os.path.exists(file_path):
+            t0 = time.time()
+            cmd = path_twitch_cli + ' -m VideoDownload' \
+                  + ' --id ' + str(video['helix']['id']) + ' --ffmpeg-path "' + path_twitch_ffmpeg + '"' \
+                  + ' --temp-path "' + path_temp + '" -o "' + file_path + '"'
+            print("CMD: " + str(cmd))
+            subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+            # subprocess.Popen(cmd, shell=True).wait()
+            print("\t- done in " + str(time.time() - t0) + " seconds")
+
+        # CHAT: check if the file exists
+        file_path_chat = path_data + export_folder + filename_format + "_chat.json"
+        file_path_chat_tmp = path_temp + str(video['helix']['id']) + "_chat.json"
+        print("\t- download chat: " + file_path_chat)
+        if not utils.terminated_requested and not os.path.exists(file_path_chat):
+            t0 = time.time()
+            cmd = path_twitch_cli + ' -m ChatDownload' \
+                  + ' --ffmpeg-path "' + path_twitch_ffmpeg + '"' \
+                  + ' --id ' + str(video['helix']['id']) + ' --embed-emotes' \
+                  + ' -o ' + file_path_chat_tmp
+            subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+            # subprocess.Popen(cmd, shell=True).wait()
+            if os.path.exists(file_path_chat_tmp):
+                shutil.move(file_path_chat_tmp, file_path_chat) 
+            print("\t- done in " + str(time.time() - t0) + " seconds")
+
+        # AUDIO-TO-TEXT: check if file exists
+        file_path_webvtt = path_data + export_folder + filename_format + ".vtt"
+        if not utils.terminated_requested and os.path.exists(file_path) and not os.path.exists(file_path_webvtt) and render_webvtt[idx]:
+            print("\t- transcribing: " + file_path_webvtt)
+            t0 = time.time()
+
+            # open the model
+            SetLogLevel(-1)
+            sample_rate = 16000
+            model = Model(path_model)
+            rec = KaldiRecognizer(model, sample_rate)
+            rec.SetWords(True)
+
+            # open ffmpeg pipe stream of the audio file (from video)
+            command = [path_twitch_ffmpeg, '-nostdin', '-loglevel', 'quiet', '-i', file_path,
+                       '-ar', str(sample_rate), '-ac', '1', '-f', 's16le', '-']
+            # process = subprocess.Popen(command, stdout=subprocess.PIPE)
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+            results = []
+            while True:
+                data = process.stdout.read(4000)
+                if len(data) == 0:
+                    break
+                if rec.AcceptWaveform(data):
+                    text = rec.Result()
+                    results.append(text)
+            results.append(rec.FinalResult())
+
+            # convert to standard format
+            vtt = WebVTT()
+            for i, res in enumerate(results):
+                words = json.loads(res).get('result')
+                if not words:
+                    continue
+                for word in words:
+                    start = utils.webvtt_time_string(word['start'])
+                    end = utils.webvtt_time_string(word['end'])
+                    vtt.captions.append(Caption(start, end, word['word']))
+            vtt.save(file_path_webvtt)
+            print("\t- done in " + str(time.time() - t0) + " seconds")
+
+            # send pushover that this twitch vod is ready to edit
+            text = video['helix']['user_name'] + " vod " + str(video['helix']['id']) \
+                    + " ready to edit (" + str(int((time.time() - t0_start)/60.0)) + " min to prepare)"
+            utils.send_pushover_message(auth, text)
+
+        # RENDER: check if the file exists
+        file_path_chat = path_data + export_folder + filename_format + "_chat.json"
+        file_path_render = path_data + export_folder + filename_format + "_chat.mp4"
+        file_path_render_tmp = path_temp + str(video['helix']['id']) + "_chat.mp4"
+        if not utils.terminated_requested and os.path.exists(file_path_chat) and not os.path.exists(file_path_render) and render_chat[idx]:
+            print("\t- rendering chat: " + file_path_render)
+            t0 = time.time()
+            cmd = path_twitch_cli + ' -m ChatRender' \
+                  + ' -i ' + file_path_chat + ' --ffmpeg-path "' + path_twitch_ffmpeg + '"' \
+                  + ' -h 926 -w 274 --update-rate 0.1 --framerate 60 --font-size 15' \
+                  + ' --temp-path "' + path_temp + '" -o ' + file_path_render_tmp
+            # subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+            subprocess.Popen(cmd, shell=True).wait()
+            if os.path.exists(file_path_render_tmp):
+                shutil.move(file_path_render_tmp, file_path_render) 
+            print("\t- done in " + str(time.time() - t0) + " seconds")
