fa5d239d8abde63c0a8ba0a452ed46e9d80d8fe1
cr08 <Vchat20@gmail.com>
2022-11-04 21:44:30 -0400

    Update 0_main_clips.py
    Removing CMD debug line for now.

--- 0_main_clips.py
+++ 0_main_clips.py
@@ -179,7 +179,7 @@ for idx, user in enumerate(users):
                       + ' --id ' + str(video['id']) + ' --ffmpeg-path "' + path_twitch_ffmpeg + '"' \
                       + ' -o ' + file_path_tmp
                       #+ ' --temp-path "' + path_root + '/TEMP/" --quality 1080p60 -o ' + file_path
-                print("CMD: " + str(cmd))
+                # print("\t- CMD: " + str(cmd))
                 # subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
                 subprocess.Popen(cmd, shell=True).wait()
                 shutil.move(file_path_tmp, file_path) 
